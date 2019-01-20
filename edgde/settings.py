f4c6df8d5a54e659153fcb1f8263c52dfb357240
Sam Svindland <sam@Sams-MacBook-Pro.local>
2019-01-19 17:21:11 -0800

    i forgot what i did

--- edgde/settings.py
+++ edgde/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'video.apps.VideoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
